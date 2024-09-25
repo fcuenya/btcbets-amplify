77235b1f4737c2aabc3a02ee0433ea47c6de11bc
Francisco Cuenya Folgar <francisco.cuenya@gmail.com>
2024-09-25 16:32:06 +0100

    fixes component test

--- src/packages/game/Game.cy.tsx
+++ src/packages/game/Game.cy.tsx
@@ -20,9 +20,12 @@ describe(
       cy.get("button.invisible").should("be.disabled");
       cy.get("button.invisible").contains("Retry");
       cy.get("button").contains("Lower").should("exist");
-      cy.get(".radial-progress").contains("is LOWER than").should("not.exist");
+      cy.get(".radial-progress .price").contains("--").should("not.exist");
+      cy.get(".radial-progress .bet")
+        .contains("is LOWER than")
+        .should("not.exist");
       cy.get("button").contains("Lower").click();
-      cy.get(".radial-progress").contains("is LOWER than").should("exist");
+      cy.get(".radial-progress .bet").contains("is LOWER than").should("exist");
       cy.get("button").should("be.disabled");
     });
   }
